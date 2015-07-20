4f5a54b0f41109974a5580d77cb29c01f70bed08
Hoang Phan <pquochoang2007@gmail.com>
2015-07-21 00:14:50 +0700

    Change View

--- image_viewer/static/src/js/viewer.js
+++ image_viewer/static/src/js/viewer.js
@@ -5,14 +5,14 @@ openerp.image_viewer = function (instance) {
     var mail = instance.mail;
 
     var load_scripts = _.once(function () {
-        instance.web.loadJS('/cosmos_image_viewer/static/lib/fancybox/lib/jquery.mousewheel-3.0.6.pack.js');
-        instance.web.loadCSS("/cosmos_image_viewer/static/lib/fancybox/source/jquery.fancybox.css?v=2.1.5");
-        instance.web.loadJS("/cosmos_image_viewer/static/lib/fancybox/source/jquery.fancybox.pack.js?v=2.1.5");
-        instance.web.loadCSS("/cosmos_image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5");
-        instance.web.loadJS("/cosmos_image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5");
-        instance.web.loadJS("/cosmos_image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6");
-        instance.web.loadCSS("/cosmos_image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7");
-        instance.web.loadJS("/cosmos_image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7");
+        instance.web.loadJS('/image_viewer/static/lib/fancybox/lib/jquery.mousewheel-3.0.6.pack.js');
+        instance.web.loadCSS("/image_viewer/static/lib/fancybox/source/jquery.fancybox.css?v=2.1.5");
+        instance.web.loadJS("/image_viewer/static/lib/fancybox/source/jquery.fancybox.pack.js?v=2.1.5");
+        instance.web.loadCSS("/image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5");
+        instance.web.loadJS("/image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5");
+        instance.web.loadJS("/image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6");
+        instance.web.loadCSS("/image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7");
+        instance.web.loadJS("/image_viewer/static/lib/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7");
     });
 
     mail.MessageCommon.include({
@@ -39,18 +39,27 @@ openerp.image_viewer = function (instance) {
         load_base64: function () {
             var self = this;
             var model = new instance.Model('ir.attachment');
-            var allow_types = ['image/png', 'image/jpeg', 'image/pjpeg', 'image/gif'];
+            var allow_types = {
+                png: 'image/png',
+                jpeg: 'image/jpeg',
+                jpg: 'image/jpeg',
+                gif: 'image/gif'
+            };
             _.each(this.attachment_ids, function (attachment) {
-                model.query(['datas', 'mimetype', 'name']).filter([
+                model.query(['datas', 'datas_fname', 'name']).filter([
                     ['id', '=', attachment.id],
-                    ['mimetype', 'in', allow_types]
                 ]).all().then(function (r) {
+                    r = _.filter(r, function(rr) {
+                        var res = /.*\.(\w+)$/.exec(rr.name);
+                        return ~_.indexOf(_.keys(allow_types), res != null ? res[1] : '');
+                    });
                     var render = _.after(r.length, self.build_wg.bind(self));
                     _.each(r, function (r) {
+                        var res = /.*\.(\w+)$/.exec(r.name);
                         var img = $(_.str.sprintf("img[src*='id=%s']", r.id), self.$(".oe_msg_attachment_list"));
                         if (img.length) {
                             var identity = _.uniqueId('fbx_');
-                            var href = _.str.sprintf("data:%s;base64,%s", r.mimetype, r.datas);
+                            var href = _.str.sprintf("data:%s;base64,%s", allow_types[res[1]], r.datas);
                             img.parent()
                                 .attr('id', identity)
                                 .removeAttr('target')
